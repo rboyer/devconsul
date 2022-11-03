3dcf7e48d911dd86791047bc5f4db8adb63ec7ff
R.B. Boyer <rb@hashicorp.com>
2022-11-03 09:27:59 -0500

    fix test

--- config/config_test.go
+++ config/config_test.go
@@ -15,7 +15,7 @@ func TestParseConfig_EmptyInferDefaults(t *testing.T) {
 		ConfName:             "legacy",
 		ConsulImage:          "consul-dev:latest",
 		EnvoyLogLevel:        "info",
-		EnvoyVersion:         "v1.22.0",
+		EnvoyVersion:         "v1.23.1",
 		TopologyNetworkShape: "flat",
 		TopologyLinkMode:     "federate",
 		TopologyClusters: []*Cluster{
